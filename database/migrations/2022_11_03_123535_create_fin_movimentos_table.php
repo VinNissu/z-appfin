2a9b960bdc57f9790b0b69311f1104418846bc70
VinnyAlmeidaBR <110014684+VinnyAlmeidaBR@users.noreply.github.com>
2022-11-03 11:47:38 -0300

    version0.5

--- database/migrations/2022_11_03_123535_create_fin_movimentos_table.php
+++ database/migrations/2022_11_03_123535_create_fin_movimentos_table.php
@@ -13,9 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        schema::create('fin_movimentos',function(){
-            
-        });
+    Schema::create('fin_movimentos',function(Blueprint $table){
+        $table->id();
+        $table->text('descricao');
+        $table->float('valor',12,2);
+        $table->string('tipo');
+        $table->ipAddress('ipuser');
+        $table->timestamps();    
+    });
     }
 
     /**
@@ -25,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('fin_movimentos');
     }
 };
