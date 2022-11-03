97eeae49d59103913a7a7333cbc885c9e52e8208
VinnyAlmeidaBR <110014684+VinnyAlmeidaBR@users.noreply.github.com>
2022-11-03 10:02:51 -0300

    version0.3

--- database/migrations/2022_11_03_123535_create_fin_movimentos_table.php
+++ database/migrations/2022_11_03_123535_create_fin_movimentos_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        schema::create('fin_movimentos',function(){
+            
+        });
     }
 
     /**
